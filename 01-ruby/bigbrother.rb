146120cd53afbde93efe8d4db74f099012f73f77
Cold Head <certainthings@gmail.com>
2014-09-08 16:01:44 +1000

    i/o examples

---
+++ 01-ruby/bigbrother.rb
@@ -0,0 +1,13 @@
+puts "Welcome to the Big Brother Database"
+
+print "What is your first name? "
+first = gets.chomp
+
+print "What is your surname? "
+surname = gets.chomp
+
+puts "Your full name is #{ first } #{ surname }"
+
+print "What is your address? "
+address = gets.chomp
+puts "Your name is #{ first } #{ surname } and you live at #{ address }"
---
+++ 01-ruby/namer.rb
@@ -0,0 +1,10 @@
+puts "Hello sailor"
+
+name = "Pablo"
+puts "My name is #{ name }"
+
+print "Enter your name: "
+name = gets.chomp # gets reads input from the keyboard.
+puts "Your name is #{ name.upcase } IN CAPS"
+
+puts "Thank you for using this program"
