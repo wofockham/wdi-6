9eceb065e8248fcfaefc74858dc505ac25a55f99
Cold Head <certainthings@gmail.com>
2014-09-09 15:21:49 +1000

    functions

--- 01-ruby/aircon.rb
+++ 01-ruby/aircon.rb
@@ -7,15 +7,20 @@ working_air_con = (gets.chomp.downcase == 'y') # True is Y or y, false otherwise
 print "What is the desired temperature? "
 desired_temperature = gets.chomp.to_i
 
-# There are many different ways to structure these statements:
-if working_air_con
-  if current_temperature > desired_temperature
-    puts "Turn on the A/C Please"
-  end
-else
-  if current_temperature > desired_temperature
-    puts "Fix the A/C now! It's hot!"
-  elsif current_temperature < desired_temperature
-    puts "Fix the A/C whenever you have the chance... It's cool..."
+def adjust_aircon(operational, current, desired)
+  # There are many different ways to structure these statements:
+  if operational
+    if current > desired
+      puts "Turn on the A/C Please"
+    end
+  else
+    if current > desired
+      puts "Fix the A/C now! It's hot!"
+    elsif current < desired
+      puts "Fix the A/C whenever you have the chance... It's cool..."
+    end
   end
 end
+
+adjust_aircon(working_air_con, current_temperature, desired_temperature)
+
