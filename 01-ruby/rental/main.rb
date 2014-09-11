205fa250d08d8a8c7622e1366322d487c2f55a79
Cold Head <certainthings@gmail.com>
2014-09-11 16:22:58 +1000

    basic menu

--- 01-ruby/rental/main.rb
+++ 01-ruby/rental/main.rb
@@ -5,12 +5,47 @@ require_relative './apartment.rb'
 require_relative './building.rb'
 
 chris = Tenant.new('Chris Eccleston', 23, 'male')
-puts chris
 
 apt = Apartment.new('25b', 100, 1, 15)
-puts apt
 
 building = Building.new('123 Fake St', 'Neo-industrial', true, false, 1000)
-puts building
 
-binding.pry
+# Globals to store our data
+$buildings = []
+$tenants = []
+
+# Seed data
+$buildings.push building
+$buildings.first.apartments[apt.name] = apt
+$buildings.first.apartments[apt.name].tenants << chris
+
+def menu
+  puts "1. List all apartments"
+  puts "q. Quit"
+end
+
+def read_selection
+  menu
+  gets.chomp.downcase
+end
+
+menu_option = read_selection
+
+until menu_option == 'q'
+  case menu_option
+  when '1'
+    # List all apartments.
+    $buildings.each do |b|
+      b.apartments.each do |number, apartment|
+        puts apartment
+      end
+    end
+
+    menu_option = read_selection # Thanks Nick!
+  end
+end
+
+
+
+
+
