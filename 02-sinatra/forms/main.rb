c83471cd29d5be34d078333c0f25a424c15999ab
Cold Head <certainthings@gmail.com>
2014-09-17 15:26:49 +1000

    search example

--- 02-sinatra/forms/main.rb
+++ 02-sinatra/forms/main.rb
@@ -12,3 +12,22 @@ end
 get '/faq' do
   erb :faq
 end
+
+get '/process' do
+  @result = "#{ params['hotdog_count']} #{ params['hotdogs']} hotdogs consumed on #{ params['start_date'] }"
+  erb "MUAHAHAHAHAHAH GROWTH HACKING"
+end
+
+get '/search' do
+  erb :search
+end
+
+get '/search_results' do
+  erb "You searched for #{ params['search_string'] }"
+end
+
+
+
+
+
+
