e2e7a63ee29998cf79586e104ae39b63d3260962
Cold Head <certainthings@gmail.com>
2014-09-19 15:32:14 +1000

    receipts database

--- 02-sinatra/receipts/main.rb
+++ 02-sinatra/receipts/main.rb
@@ -7,5 +7,20 @@ get '/' do
 end
 
 post '/' do
+  db = File.open('receipts.csv', 'a+')
+  db.puts params.values.join(',')
+  db.close
   erb :receipt
+end
+
+get '/all' do
+  db = File.open('receipts.csv', 'r')
+
+  # Convert "lines" into an array of arrays so we can close the file.
+  @receipts = db.map do |line|
+    line.split ','
+  end
+
+  db.close
+  erb :all
 end
