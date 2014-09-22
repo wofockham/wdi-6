69aa1ce0d79ec27e62380dbe8542c8aa8d963aac
Cold Head <certainthings@gmail.com>
2014-09-22 12:01:31 +1000

    families.sql

---
+++ 03-sql/families.sql
@@ -0,0 +1,25 @@
+CREATE TABLE person (
+  id INTEGER PRIMARY KEY,
+  first_name TEXT,
+  last_name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pet (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  breed TEXT,
+  age INTEGER,
+  dead INTEGER
+);
+
+CREATE TABLE person_pet (
+  person_id INTEGER,
+  pet_id INTEGER
+);
+
+INSERT INTO person (id, first_name, last_name, age) VALUES (0, 'Zed', 'Shaw', 37);
+
+INSERT INTO pet (id, name, breed, age, dead) VALUES (0, 'Fluffy', 'Unicorn', 1000, 0);
+INSERT INTO pet VALUES (1, 'Gigantor', 'Robot', 1, 1);
+
