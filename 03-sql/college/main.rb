8774f8584e3f76196041a387032b63a51070225f
Cold Head <certainthings@gmail.com>
2014-09-24 09:57:38 +1000

    college db

---
+++ 03-sql/college/main.rb
@@ -0,0 +1,13 @@
+require 'pry'
+require 'active_record'
+
+ActiveRecord::Base.logger = Logger.new(STDERR)
+
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :database => 'college.db'
+)
+
+require_relative 'student'
+
+binding.pry
---
+++ 03-sql/college/students.sql
@@ -0,0 +1,23 @@
+CREATE TABLE students (
+  id INTEGER AUTO INCREMENT PRIMARY KEY,
+  first TEXT NOT NULL,
+  last TEXT NOT NULL,
+  dob DATE,
+  gpa DECIMAL
+);
+
+CREATE TABLE courses (
+  id INTEGER AUTO INCREMENT PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+INSERT INTO students (first, last, dob, gpa) VALUES ('bill', 'jones', '1/1/1990', 3.3);
+INSERT INTO students (first, last, dob, gpa) VALUES ('janet', 'jones', '2/1/1990', 3.7);
+INSERT INTO students (first, last, dob, gpa) VALUES ('jill', 'smith', '2/1/1980', 3.5);
+INSERT INTO students (first, last, dob, gpa) VALUES ('tim', 'smith', '2/1/1990', 3.6);
+INSERT INTO students (first, last, dob, gpa) VALUES ('january', 'jones', '2/1/1990', 3.7);
+INSERT INTO students (first, last, dob, gpa) VALUES ('jim', 'jones', '2/1/1980', 3.5);
+
+
+INSERT INTO courses (id,name) VALUES (1,'WDI Web Development');
+INSERT INTO courses (id,name) VALUES (2,'WDI Front End');
