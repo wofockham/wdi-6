8e2fe2eca1dced724a7ef64f65c5a98ce2ace430
Cold Head <certainthings@gmail.com>
2014-09-30 15:11:19 +1000

    just like television

--- 05-rails/solarsystem/app/controllers/planets_controller.rb
+++ 05-rails/solarsystem/app/controllers/planets_controller.rb
@@ -1,8 +1,25 @@
 class PlanetsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def index
     @planets = Planet.all
   end
 
   def new
+  end
+
+  def create
+    Planet.create planet_params
+    redirect_to(planets_path)
+  end
+
+  def show
+    @planet = Planet.find params[:id]
+  end
+
+  private
+  def planet_params
+    # Strong params aka params whitelisting
+    params.require(:planet).permit(:name, :image, :orbit, :diameter, :mass, :moons)
   end
 end
--- 05-rails/solarsystem/config/routes.rb
+++ 05-rails/solarsystem/config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root :to => 'planets#index'
   get '/planets' => 'planets#index'
   get '/planets/new' => 'planets#new'
+  post '/planets' => 'planets#create'
+  get '/planets/:id' => 'planets#show', :as => 'planet'
+  post '/planets/:id/delete' => 'planets#destroy', :as => 'planet_delete'
 end
