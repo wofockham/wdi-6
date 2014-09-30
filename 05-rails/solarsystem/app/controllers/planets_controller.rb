b8e31c8f62f5a51c844aee1fd8bff56ad4c498a9
Cold Head <certainthings@gmail.com>
2014-09-30 14:29:00 +1000

    new planet form

--- 05-rails/solarsystem/app/controllers/planets_controller.rb
+++ 05-rails/solarsystem/app/controllers/planets_controller.rb
@@ -2,4 +2,7 @@ class PlanetsController < ApplicationController
   def index
     @planets = Planet.all
   end
+
+  def new
+  end
 end
--- 05-rails/solarsystem/config/routes.rb
+++ 05-rails/solarsystem/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root :to => 'planets#index'
   get '/planets' => 'planets#index'
+  get '/planets/new' => 'planets#new'
 end
