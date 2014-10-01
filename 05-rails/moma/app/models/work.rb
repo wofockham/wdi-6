659069c84086d1511b7cf774f707c6f131856d67
Cold Head <certainthings@gmail.com>
2014-10-01 12:03:25 +1000

    controller skellingtons

---
+++ 05-rails/moma/app/models/work.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: works
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  year       :string(255)
+#  medium     :string(255)
+#  style      :string(255)
+#  image      :text
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class Work < ActiveRecord::Base
+end
--- 05-rails/moma/db/seeds.rb
+++ 05-rails/moma/db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.destroy_all
+Work.destroy_all
+
+Artist.create(:name => 'Joan Miro', :nationality => 'Spanish', :dob => '1893/04/20', :period => '20th century', :image => 'http://upload.wikimedia.org/wikipedia/commons/5/5c/Portrait_of_Joan_Miro%2C_Barcelona_1935_June_13.jpg')
+Work.create(:title => 'The Flight of the Dragonfly in Front of the Sun', :year => '1968', :medium => 'oil on canvas', :style => 'Abstract Art', :image => 'http://uploads0.wikipaintings.org/images/joan-miro/the-flight-of-the-dragonfly-in-front-of-the-sun.jpg')
