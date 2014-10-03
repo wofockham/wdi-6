2e75d92c4e5a4d0957454792f00bff6cb0f6bc9a
Cold Head <certainthings@gmail.com>
2014-10-03 12:20:01 +1000

    user index

--- 05-rails/tunr/app/controllers/users_controller.rb
+++ 05-rails/tunr/app/controllers/users_controller.rb
@@ -13,6 +13,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find params[:id]
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
