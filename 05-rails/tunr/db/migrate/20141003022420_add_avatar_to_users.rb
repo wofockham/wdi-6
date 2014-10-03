058cc8e9afdd4409d10d7cce2c89e2b7765c2501
Cold Head <certainthings@gmail.com>
2014-10-03 12:27:35 +1000

    default avatar

--- 05-rails/tunr/db/migrate/20141003022420_add_avatar_to_users.rb
+++ 05-rails/tunr/db/migrate/20141003022420_add_avatar_to_users.rb
@@ -1,5 +1,5 @@
 class AddAvatarToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :avatar, :text
+    add_column :users, :avatar, :text, :default => 'http://derpreview.files.wordpress.com/2012/01/harpo-marx-02.jpg'
   end
 end
