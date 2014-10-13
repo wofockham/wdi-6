bcc533f704dd368246ec4899ef3cf4d6077f9ffd
Cold Head <certainthings@gmail.com>
2014-10-13 16:02:30 +1100

    WHATEVER

--- 05-rails/pinterest/app/controllers/pinterest_controller.rb
+++ 05-rails/pinterest/app/controllers/pinterest_controller.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 
 class PinterestController < ApplicationController
   def results
-    doc = Nokogiri::HTML(open("http://www.pinterest.com/search/pins/?q=#{ params[:query] }"))
+    @doc = Nokogiri::HTML(open("http://www.pinterest.com/search/pins/?q=#{ params[:query] }"))
     images = doc.css('img.pinImg')
     @urls = images.map do |i|
       i.get_attribute('src')
