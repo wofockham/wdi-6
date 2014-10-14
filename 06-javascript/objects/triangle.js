66251b500c248459439afba24794e9dca7d9d5a3
Cold Head <certainthings@gmail.com>
2014-10-14 14:43:49 +1100

    isNaN()

--- 06-javascript/objects/triangle.js
+++ 06-javascript/objects/triangle.js
@@ -1,17 +1,19 @@
 var area = function(triangle) {
-  var valid = function(t) {
-    return t.sideA + t.sideB > t.sideC &&
-           t.sideB + t.sideC > t.sideA &&
-           t.sideC + t.sideA > t.sideB;
-  };
-
-  if (! valid(triangle)) {
-    return null;
-  }
-
   // via Heron's Formula
   var s = (triangle.sideA + triangle.sideB + triangle.sideC) / 2;
   var a = Math.sqrt(s * (s - triangle.sideA) * (s - triangle.sideB) * (s - triangle.sideC));
 
   return a;
 };
+
+var invalid = {
+  sideA: 1,
+  sideB: 1,
+  sideC: 10000
+}
+
+var invalid_area = area(invalid);
+
+if (isNaN(invalid_area)) {
+  alert('INVALID TRIANGLE DETECTED');
+}
