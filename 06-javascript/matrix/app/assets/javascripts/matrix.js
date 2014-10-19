aa590c50e312f759988a96210481216934c06466
Cold Head <certainthings@gmail.com>
2014-10-20 10:21:55 +1100

    you watching this veronique

--- 06-javascript/matrix/app/assets/javascripts/matrix.js
+++ 06-javascript/matrix/app/assets/javascripts/matrix.js
@@ -1,1 +1,17 @@
-console.log('hello world', $, _);
+$(document).ready(function () {
+  $('#add_number').on('click', addNumber);
+  $('#square').on('click', square);
+});
+
+var numbers = [];
+
+var addNumber = function () {
+  var number = parseInt( $('#number').val() );
+  numbers.push(number);
+
+  var $box = $('<div></div>').addClass('box');
+  $box.text( number );
+  $('#boxes').prepend($box);
+
+  $('#number').val('').focus();
+};
