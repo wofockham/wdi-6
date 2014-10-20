ac5035755bce2076a209506c2944bcfda02ce0cf
Cold Head <certainthings@gmail.com>
2014-10-20 11:00:18 +1100

    eval, so evil

--- 06-javascript/matrix/app/assets/javascripts/matrix.js
+++ 06-javascript/matrix/app/assets/javascripts/matrix.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   $('#add_number').on('click', addNumber);
   $('#square').on('click', square);
+  $('#calculate').on('click', calculate);
 });
 
 var numbers = [];
@@ -9,9 +10,42 @@ var addNumber = function () {
   var number = parseInt( $('#number').val() );
   numbers.push(number);
 
-  var $box = $('<div></div>').addClass('box');
-  $box.text( number );
-  $('#boxes').prepend($box);
+  showBox(number);
 
   $('#number').val('').focus();
+};
+
+var square = function () {
+  var squareValue = function (i) {
+    return i * i;
+  };
+
+  numbers = _(numbers).map(squareValue);
+  showBoxes();
+};
+
+var calculate = function () {
+  numbers = _(numbers).map(funkyCalc);
+  showBoxes();
+};
+
+var funkyCalc = function (i) {
+  var equation = $('#number').val();
+  return eval(equation);
+};
+
+
+var showBox = function (i) {
+  var $box = $('<div></div>').addClass('box');
+  $box.text( i );
+  $('#boxes').prepend($box);
+};
+
+var showBoxes = function () {
+  emptyBoxes();
+  _(numbers).each(showBox);
+};
+
+var emptyBoxes = function () {
+  $('#boxes').empty();
 };
