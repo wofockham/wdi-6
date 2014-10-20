d26f94cb85448ba291173360439a30c44f323ec6
Cold Head <certainthings@gmail.com>
2014-10-21 10:04:59 +1100

    andrewwww

--- 07-ajax/ajax-intro/main.rb
+++ 07-ajax/ajax-intro/main.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
 
 get '/' do
   erb :index
@@ -13,3 +14,19 @@ get '/lotto' do
   sleep 3
   Random.rand(40).to_s
 end
+
+get '/hi' do
+  "<h2>Hello world</h2>"
+end
+
+get '/hi.json' do
+  content_type :json
+  { :word1 => 'Hello', :word2 => 'World' }.to_json
+end
+
+get '/time.json' do
+  content_type :json
+  { :now => Time.now, :lucky_number => Random.rand(100) }.to_json
+end
+
+
--- 07-ajax/ajax-intro/public/js/lotto.js
+++ 07-ajax/ajax-intro/public/js/lotto.js
@@ -1,3 +1,20 @@
+$(document).ready(function () {
+  setInterval(function () {
+    $.ajax('/time.json').done(function (stamp) {
+      $('body').html('<p>Current time:' + stamp.now + '</p>');
+      $('<p></p>').text('Lucky number:' + stamp.lucky_number).appendTo('body');
+    });
+  }, 1000);
+});
+
+// $(document).ready(function () {
+//   $.ajax('/hi.json').done(function (result) {
+//     var $heading = $('<p></p>');
+//     $heading.text( result.word2 + ' ' + result.word1 );
+//     $('body').append($heading);
+//   });
+// });
+
 // var request = new XMLHttpRequest();
 // request.open('GET', '/lotto', true);
 // request.send();
@@ -6,16 +23,16 @@
 // console.log("received", request.responseText);
 // alert(request.responseText);
 
-var slowRequest = function () {
-  var request = new XMLHttpRequest();
-  request.open('GET', '/lotto', true);
-  request.onreadystatechange = function () {
-    console.log("readyState updated", request.readyState);
-    if (request.readyState === 4) {
-      alert(request.responseText);
-    }
-  }
-  request.send();
-};
+// var slowRequest = function () {
+//   var request = new XMLHttpRequest();
+//   request.open('GET', '/lotto', true);
+//   request.onreadystatechange = function () {
+//     console.log("readyState updated", request.readyState);
+//     if (request.readyState === 4) {
+//       alert(request.responseText);
+//     }
+//   }
+//   request.send();
+// };
 
-slowRequest();
+// slowRequest();
