01ba8300f726fd447c7e088259831a0c1a3f8262
Cold Head <certainthings@gmail.com>
2014-10-21 11:33:12 +1100

    flickr url is happy at last

--- 07-ajax/flickr_app/app/assets/javascripts/flickr.js
+++ 07-ajax/flickr_app/app/assets/javascripts/flickr.js
@@ -4,14 +4,13 @@ $(document).ready(function () {
 
 var searchFlickr = function (event) {
   var query = $('#query').val();
-  var flickrUrl = 'https://api.flickr.com/services/rest/';
+  var flickrUrl = 'https://api.flickr.com/services/rest/?jsoncallback=?';
   $.getJSON(flickrUrl, {
     method: 'flickr.photos.search',
     api_key: '2f5ac274ecfac5a455f38745704ad084',
     text: query,
-    format: 'json',
-    jsoncallback: 'processImages'
-  });
+    format: 'json'
+  }, processImages);
   console.log(flickrUrl);
 };
 
