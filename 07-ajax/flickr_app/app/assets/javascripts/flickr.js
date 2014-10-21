ebe18f527379216a2b58668a9d3a980d4707cc6c
Cold Head <certainthings@gmail.com>
2014-10-21 11:15:49 +1100

    flickrrr

---
+++ 07-ajax/flickr_app/app/assets/javascripts/flickr.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $('#search').on('click', searchFlickr);
+});
+
+var searchFlickr = function (event) {
+  var query = $('#query').val();
+  var flickrUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=2f5ac274ecfac5a455f38745704ad084&text=' + query;
+  console.log(flickrUrl);
+};
