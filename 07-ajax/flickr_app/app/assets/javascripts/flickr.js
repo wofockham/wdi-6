09e545f647abebf78c7fab4e66830a27323e2e31
Cold Head <certainthings@gmail.com>
2014-10-21 12:14:50 +1100

    crappy infinite scroll

--- 07-ajax/flickr_app/app/assets/javascripts/flickr.js
+++ 07-ajax/flickr_app/app/assets/javascripts/flickr.js
@@ -1,19 +1,70 @@
 $(document).ready(function () {
   $('#search').on('click', searchFlickr);
+  $('#clear').on('click', clearPhotos);
+  $(window).on('scroll', maybeLoad);
 });
+
+var requestInProgress = false;
+
+var maybeLoad = function () {
+  if (requestInProgress) {
+    return; // Abort!
+  }
+
+  var scrollTop = $(window).scrollTop();
+  var docHeight = $(document).height();
+  var windowHeight = $(window).height();
+
+  if (scrollTop > docHeight - (3 * windowHeight)) {
+    searchFlickr();
+  }
+};
+
+var clearPhotos = function () {
+  $('#images').empty();
+};
 
 var searchFlickr = function (event) {
   var query = $('#query').val();
   var flickrUrl = 'https://api.flickr.com/services/rest/?jsoncallback=?';
+  requestInProgress = true;
+
   $.getJSON(flickrUrl, {
     method: 'flickr.photos.search',
     api_key: '2f5ac274ecfac5a455f38745704ad084',
     text: query,
     format: 'json'
   }, processImages);
-  console.log(flickrUrl);
 };
 
 var processImages = function (result) {
-  debugger;
+  requestInProgress = false;
+
+  if (result.stat != 'ok') {
+    return;
+  }
+
+  _(result.photos.photo).each(function (photo) {
+    // https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+    var url = [
+      'https://farm',
+      photo.farm,
+      '.staticflickr.com/',
+      photo.server,
+      '/',
+      photo.id,
+      '_',
+      photo.secret,
+      '_q.jpg'
+    ].join('');
+
+    $('<img>', {
+      src: url
+    }).appendTo('#images');
+
+  });
 };
+
+
+
+
