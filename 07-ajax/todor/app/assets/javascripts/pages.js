81c28ff5d9c9f81d6fe7c76cc5d171017b719ece
Cold Head <certainthings@gmail.com>
2014-10-22 11:48:25 +1100

    stupid jquery tricks

---
+++ 07-ajax/todor/app/assets/javascripts/pages.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+
+  // Parallax Bill Murray
+  var $hero = $('.hero');
+  $(window).on('scroll', function () {
+    var scrollTop = $(window).scrollTop();
+    var bgPosition = scrollTop / 2;
+    if (bgPosition > 0) {
+      $hero.css('background-position-y', -bgPosition);
+    }
+  });
+
+  // Bubbles
+  $(window).on('mousemove', function (e) {
+    var size = Math.random() * 4;
+    var $bubble = $('<div></div>');
+    $bubble.addClass('bubble');
+    $bubble.css({
+      left: e.pageX,
+      top: e.pageY,
+      width: size + 'em',
+      height: size + 'em'
+    });
+    $bubble.appendTo('body');
+
+    setTimeout(function () {
+      $bubble.animate({top: '-10em'}, 3000, function () {
+        $bubble.remove();
+      });
+    }, 1000);
+  });
+
+});
