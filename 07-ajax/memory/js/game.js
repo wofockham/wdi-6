a4904841b5d73f08fa72130f130603804866317e
Cold Head <certainthings@gmail.com>
2014-10-23 12:56:28 +1100

    cards

---
+++ 07-ajax/memory/js/game.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  var numbers = [1, 1, 2, 2, 3, 3];
+  numbers = _(numbers).shuffle();
+
+  console.log(numbers);
+
+  _(numbers).each(function (i) {
+    var template = $('#cardTemplate').html();
+    var cardHTML = Handlebars.compile(template);
+
+    $('body').append( cardHTML({number: i}) );
+  });
+});
