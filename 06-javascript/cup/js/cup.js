56bc6b79f1ad87eca524017ec398a43b3d3f175e
Cold Head <certainthings@gmail.com>
2014-11-04 11:24:50 +1100

    long stupid name

--- 06-javascript/cup/js/cup.js
+++ 06-javascript/cup/js/cup.js
@@ -8,12 +8,39 @@ var cup = {
   sweep5: {},
 
   init: function () {
+    cup.reload(); // Fetch a previously saved dataset, if available.
+
     // Create new copies of the field for each of our sweeps.
     this.field2 = _(this.field).shuffle();
     this.field5 = _(this.field).shuffle();
 
     cup.showField();
     cup.showSweepsRemaining();
+    cup.showPrizes();
+    cup.showSweepsBought();
+  },
+
+  save: function () {
+    var cupString = JSON.stringify(cup);
+    localStorage.setItem('cup', cupString);
+    console.log('saved');
+  },
+
+  reload: function () {
+    var cupString = localStorage.getItem('cup');
+    if (! cupString) {
+      return; // Nothing found in local storage.
+    }
+
+    var cupCopy = JSON.parse(cupString);
+
+    // Merge saved data into cup object.
+    cup = _.extend(cup, cupCopy);
+  },
+
+  reset: function () {
+    localStorage.removeItem('cup');
+    window.location.reload();
   },
 
   showSweepsBought: function () {
@@ -48,6 +75,35 @@ var cup = {
     });
   },
 
+  showPrizes: function () {
+    var currency = function (amount) {
+      return '$' + amount.toFixed(2);
+    };
+
+    var pool2 = this.field.length * 2;
+    var pool5 = this.field.length * 5;
+
+    var first2 = Math.ceil(0.5 * pool2);
+    var second2 = Math.ceil(0.3 * pool2);
+    var wooden2 = 2;
+    var third2 = pool2 - first2 - second2 - wooden2;
+
+    $('#prize2first').text(currency(first2));
+    $('#prize2second').text(currency(second2));
+    $('#prize2third').text(currency(third2));
+    $('#prize2wooden').text(currency(wooden2));
+
+    var first5 = Math.ceil(0.5 * pool5);
+    var second5 = Math.ceil(0.3 * pool5);
+    var wooden5 = 5;
+    var third5 = pool5 - first5 - second5 - wooden5;
+
+    $('#prize5first').text(currency(first5));
+    $('#prize5second').text(currency(second5));
+    $('#prize5third').text(currency(third5));
+    $('#prize5wooden').text(currency(wooden5));
+  },
+
   highlightHorse: function (horse) {
     $('.field ul .active').removeClass('active');
     $('.field ul').find('li:contains(' + horse + ')').addClass('active');
@@ -57,7 +113,7 @@ var cup = {
     var $this = $(this);
     var gambler = $.trim($('#gambler').val());
 
-    $('#gambler').val(''); // Clear the input.
+    $('#gambler').val('').focus(); // Clear and focus the input.
 
     if (! gambler) { // You must enter a name to buy a stake.
       return;
@@ -69,12 +125,15 @@ var cup = {
       cup.buyStake5(gambler);
     }
 
+    cup.save(); // Persist the current data to localStorage for safety.
+
     cup.showSweepsBought();
   },
 
   buyStake2: function (gambler) {
     var horse = this.field2.pop();
     if (! horse) {
+      $('#bet2').attr('disabled', 'disabled');
       alert('Sold out');
       return;
     }
@@ -88,6 +147,7 @@ var cup = {
   buyStake5: function (gambler) {
     var horse = this.field5.pop();
     if (! horse) {
+      $('#bet5').attr('disabled', 'disabled');
       alert('Sold out');
       return;
     }
