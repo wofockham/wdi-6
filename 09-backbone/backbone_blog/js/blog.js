40586b2ed8dfcf84328d18706548f350d949ba54
Cold Head <certainthings@gmail.com>
2014-11-06 10:41:02 +1100

    fixmyjs

--- 09-backbone/backbone_blog/js/blog.js
+++ 09-backbone/backbone_blog/js/blog.js
@@ -1,5 +1,7 @@
 var app = {};
 app.Views = {};
+app.Models = {};
+app.Collections = {};
 
 app.BlogRouter = Backbone.Router.extend({
   routes: {
@@ -24,6 +26,17 @@ app.BlogRouter = Backbone.Router.extend({
 });
 
 
+app.Models.Post = Backbone.Model.extend({
+  defaults: {
+    title: 'Untitled post',
+    content: 'Your ad here'
+  }
+});
+
+app.Collections.Posts = Backbone.Collection.extend({
+  model: app.Models.Post
+});
+
 app.Views.AppView = Backbone.View.extend({
   el: '#main',
   initialize: function () {
@@ -36,6 +49,13 @@ app.Views.AppView = Backbone.View.extend({
 
 
 $(document).ready(function () {
+  app.posts = new app.Collections.Posts([
+    new app.Models.Post({id: 1, title: 'First post', content: 'This is the first'}),
+    new app.Models.Post({id: 2, title: 'Second post', content: 'This is the second'}),
+    new app.Models.Post({id: 3, title: 'Third post', content: 'This is the third'}),
+    new app.Models.Post({id: 4, title: 'Fourth post', content: 'THIS IS NUMBER FOUR'})
+  ]);
+
   app.router = new app.BlogRouter();
   Backbone.history.start();
 });
