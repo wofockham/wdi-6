6bf999b1f98a552ab582353b0595052ee6cd4d41
Cold Head <certainthings@gmail.com>
2014-11-07 12:04:56 +1100

    hacks

--- 09-backbone/backbone_blog_rails/app/assets/javascripts/blog.js
+++ 09-backbone/backbone_blog_rails/app/assets/javascripts/blog.js
@@ -1,9 +1,33 @@
 $(document).ready(function () {
+  if ($('#main').length === 0) {
+    return;
+  };
+
   app.posts = new app.Collections.Posts();
 
   app.posts.fetch().done(function () {
     app.router = new app.BlogRouter();
     Backbone.history.start();
+  });
+
+  // This should all be in a backbone view.
+  $('#new_post').on('submit', function (event) {
+    event.preventDefault();
+
+    var newPost = new app.Models.Post();
+
+    newPost.set('title', $('#post_title').val());
+    newPost.set('slug', $('#post_slug').val());
+    newPost.set('content', $('#post_content').val());
+
+    newPost.save().done(function () {
+      app.posts.fetch().done(function () {
+        var appView = new app.Views.AppView({collection: app.posts});
+        appView.render();
+      });
+    });
+
+    this.reset();
   });
 
 });
