81c3448d64c72c3680ff5465b0e15def77ff8b9a
Cold Head <certainthings@gmail.com>
2014-11-17 13:05:43 +1100

    monkey patch bubble sort

---
+++ 10-advanced/algorithms/bubble_sort.rb
@@ -0,0 +1,27 @@
+class Array
+
+  def bubble_sort(target=nil)
+    sorted = target || Array.new(self) # Create a new copy of the array.
+    max = sorted.length - 1 # Upper bound: this is reduced with each iteration.
+
+    until max <= 0 # Loop until we've compared everything.
+      i = 0
+      while i < max
+        if sorted[i] > sorted[i + 1] # Out of order?
+          sorted[i], sorted[i+1] = sorted[i+1], sorted[i] # Swap
+        end
+        i += 1 # Next item.
+      end
+      max -= 1 # Reduce max bound.
+    end
+
+    sorted
+  end
+
+  def bubble_sort!
+    bubble_sort(self)
+  end
+end
+
+require 'pry'
+binding.pry
