63887792c38fa6175d0befb95e58dceaa526fa17
Cold Head <certainthings@gmail.com>
2014-11-18 11:53:40 +1100

    additional number methods in spec

--- 10-advanced/numb3rs/app/models/number.rb
+++ 10-advanced/numb3rs/app/models/number.rb
@@ -1,5 +1,12 @@
 class Number
   def self.bin2dec(bin)
-    13
+    return 0 unless bin =~ /^[01]+$/ # Reject strings with anything other than 1s and 0s.
+
+    result = 0
+    bin.chars.reverse.each_with_index do |bit, column|
+      result += bit.to_i * (2 ** column)
+    end
+
+    result
   end
 end
--- 10-advanced/numb3rs/spec/models/number_spec.rb
+++ 10-advanced/numb3rs/spec/models/number_spec.rb
@@ -3,7 +3,31 @@ require 'rails_helper'
 describe Number do
   describe '.bin2dec' do
     it 'converts binary binary to decimal' do
+      expect(Number.bin2dec('0000')).to eq 0
       expect(Number.bin2dec('1101')).to eq 13
+      expect(Number.bin2dec('1111')).to eq 15
+    end
+
+    it 'returns 0 for invalid binary strings' do
+      expect(Number.bin2dec('7 hotdogs')).to eq 0
+    end
+  end
+
+  describe '.dec2bin' do
+    it 'converts decimal to binary' do
+      expect(Number.dec2bin(13)).to eq '1101'
+    end
+  end
+
+  describe '.bin2hex' do
+    it 'converts binary to hexadecimal' do
+      expect(Number.bin2hex('1110111110011')).to eq '1df3'
+    end
+  end
+
+  describe '.hex2bin' do
+    it 'converts hexadecimal to binary' do
+      expect(Number.hex2bin('1df3')).to eq '1110111110011'
     end
   end
 end
