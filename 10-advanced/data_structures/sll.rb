ca8916d52d4b7a27e98508345709aa352d0b735e
Cold Head <certainthings@gmail.com>
2014-11-19 11:12:05 +1100

    def last

--- 10-advanced/data_structures/sll.rb
+++ 10-advanced/data_structures/sll.rb
@@ -5,15 +5,20 @@ class SinglyLinkedList
     @head = Node.new(value) if value
   end
 
-  def append(value)
-    new_node = Node.new(value)
-
+  def last
     last_node = @head
 
-    # Find the last node
     while last_node.next
       last_node = last_node.next
     end
+
+    last_node
+  end
+
+  def append(value)
+    new_node = Node.new(value)
+
+    last_node = last
 
     last_node.next = new_node
   end
