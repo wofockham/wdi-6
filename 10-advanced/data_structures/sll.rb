56c3199247d0bfb912f7ddfa294e9ee898960849
Cold Head <certainthings@gmail.com>
2014-11-19 11:28:23 +1100

    peace attack

--- 10-advanced/data_structures/sll.rb
+++ 10-advanced/data_structures/sll.rb
@@ -6,6 +6,8 @@ class SinglyLinkedList
   end
 
   def last
+    return nil unless @head
+
     last_node = @head
 
     while last_node.next
@@ -19,10 +21,41 @@ class SinglyLinkedList
     new_node = Node.new(value)
 
     last_node = last
-
-    last_node.next = new_node
+    if last_node
+      last_node.next = new_node
+    else
+      @head = new_node
+    end
   end
 
+  def insertAfter(value, new_value)
+  end
+
+  def prepend(value)
+  end
+
+  def remove # pop(): remove from the end and return the value
+  end
+
+  def find(value) # returns the node with value=value or nil if not found
+  end
+
+  def reverse # returns a new SLL with nodes in the reverse order
+  end
+
+  def reverse!
+  end
+
+  def length
+    count = 0
+    next_node = @head
+    while next_node
+      count += 1
+      next_node = next_node.next
+    end
+
+    count
+  end
 
   class Node
     attr_accessor :value, :next
